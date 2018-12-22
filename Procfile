f9cb2ed2be3f4be1bf4ee659a7373356d2b726e5
Juha Harviainen <juha.harviainen@gmail.com>
2018-12-22 04:50:48 +0200

    Reorganize files

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn --preload --workers 1 forum:app
+web: gunicorn --preload --workers 1 application:app
--- application/__init__.py
+++ application/__init__.py
@@ -1,6 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-	return render_template("index.html")
+from application import views
