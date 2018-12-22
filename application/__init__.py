1dcbe29d29006a5c7373b3ad1ce5a9a06751c921
Juha Harviainen <juha.harviainen@gmail.com>
2018-12-22 05:53:52 +0200

    Start working on a database

--- application/__init__.py
+++ application/__init__.py
@@ -1,4 +1,15 @@
 from flask import Flask
 app = Flask(__name__)
 
+from flask_sqlalchemy import SQLAlchemy
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///forum.db"
+app.config["SQLALCHEMY_ECHO"] = True
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
 from application import views
+
+from application.users import models
+
+db.create_all()
---
+++ application/users/models.py
@@ -0,0 +1,18 @@
+from application import db
+
+class User(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+
+	username = db.Column(db.String(30), nullable=False)
+	first_name = db.Column(db.String(30), nullable=False)
+	surname = db.Column(db.String(30), nullable=False)
+	password_hash = db.Column(db.String(64), nullable=False) # we will use SHA-256
+
+	admin = db.Column(db.Boolean, nullable=False)
+
+	def __init__(self, username, first_name, surname, password_hash, admin):
+		self.username = username
+		self.first_name = first_name
+		self.surname = surname
+		self.password_hash = password_hash
+		self.admin = admin
