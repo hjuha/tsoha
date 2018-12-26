e2d6ececc0ebde2ed99a20db899037794f09454d
Juha Harviainen <juha.harviainen@gmail.com>
2018-12-26 03:24:03 +0200

    Create base model

--- application/threads/models.py
+++ application/threads/models.py
@@ -1,12 +1,9 @@
 from application import db
+from application.models import Base
 
-class Thread(db.Model):
-	id = db.Column(db.Integer, primary_key = True)
-
+class Thread(Base):
 	topic = db.Column(db.String(50), nullable = False)
 	sender_id = db.Column(db.Integer, db.ForeignKey("account.id"), nullable = False)
-	date_created = db.Column(db.DateTime, default = db.func.current_timestamp())
-	date_modified = db.Column(db.DateTime, default = db.func.current_timestamp(), onupdate = db.func.current_timestamp())
 
 	posts = db.relationship("Post", backref = "thread", lazy = True)
 
@@ -14,12 +11,8 @@ class Thread(db.Model):
 		self.topic = topic
 		self.sender_id = sender_id
 
-class Post(db.Model):
-	id = db.Column(db.Integer, primary_key = True)
-
+class Post(Base):
 	content = db.Column(db.String(1000), nullable = False)
-	date_created = db.Column(db.DateTime, default = db.func.current_timestamp())
-	date_modified = db.Column(db.DateTime, default = db.func.current_timestamp(), onupdate = db.func.current_timestamp())
 
 	sender_id = db.Column(db.Integer, db.ForeignKey("account.id"), nullable = False)
 	thread_id = db.Column(db.Integer, db.ForeignKey("thread.id"), nullable = False)
