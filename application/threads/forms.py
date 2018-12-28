86315ce58f88f96ce2efe6cdbe12ff33527a14b8
Juha Harviainen <juha.harviainen@gmail.com>
2018-12-28 15:32:28 +0200

    Return a line removed by accident

--- application/threads/forms.py
+++ application/threads/forms.py
@@ -4,6 +4,7 @@ from wtforms.widgets import TextArea
 
 class ThreadForm(FlaskForm):
 	topic = StringField("Aihe", [validators.Length(min=1, max=50, message="Aiheen pituuden pitää olla 1-50 merkkiä")])
+	content = StringField("Viesti", [validators.Length(min=1, max=1000, message="Viestin pituuden pitää olla 1-1000 merkkiä")], widget = TextArea())
 
 	class Meta:
 		csrf = False
