d10b18045a796ad5767d06c0875ae2040acdecbe
Juha Harviainen <juha.harviainen@gmail.com>
2018-12-28 12:53:51 +0200

    Allow 1 character topics, categories and posts

--- application/categories/forms.py
+++ application/categories/forms.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, validators
 
 class CategoryForm(FlaskForm):
-	name = StringField("Nimi", [validators.Length(min=2, max=20, message="Kategorian nimen pitää olla 2-25 merkkiä")])
+	name = StringField("Nimi", [validators.Length(min=1, max=25, message="Kategorian nimen pitää olla 2-25 merkkiä")])
 
 	class Meta:
 		csrf = False
--- application/threads/forms.py
+++ application/threads/forms.py
@@ -3,14 +3,13 @@ from wtforms import StringField, validators
 from wtforms.widgets import TextArea
 
 class ThreadForm(FlaskForm):
-	topic = StringField("Aihe", [validators.Length(min=2, max=50, message="Aiheen pituuden pitää olla 2-50 merkkiä")])
-	content = StringField("Viesti", [validators.Length(min=2, max=1000, message="Viestin pituuden pitää olla 2-1000 merkkiä")], widget = TextArea())
+	topic = StringField("Aihe", [validators.Length(min=1, max=50, message="Aiheen pituuden pitää olla 1-50 merkkiä")])
 
 	class Meta:
 		csrf = False
 
 class PostForm(FlaskForm):
-	content = StringField("Vastaus", [validators.Length(min=2, max=1000, message="Viestin pituuden pitää olla 2-1000 merkkiä")], widget = TextArea())
+	content = StringField("Vastaus", [validators.Length(min=1, max=1000, message="Viestin pituuden pitää olla 1-1000 merkkiä")], widget = TextArea())
 
 	class Meta:
 		csrf = False
