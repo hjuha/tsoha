1ec12d5b04a7bd2591749525898c9ac39cc92dc3
Juha Harviainen <juha.harviainen@gmail.com>
2019-01-13 21:56:47 +0200

    Fix a bug in regex

--- application/auth/forms.py
+++ application/auth/forms.py
@@ -14,7 +14,7 @@ class RegisterForm(FlaskForm):
 	first_name = StringField("Etunimi", [validators.Length(min=2, max=25, message="Etunimen pituuden pitää olla 2-25 merkkiä")])
 	surname = StringField("Sukunimi", [validators.Length(min=2, max=25, message="Sukunimen pituuden pitää olla 2-25 merkkiä")])
 	password = PasswordField("Salasana", [validators.Length(min=8, message="Salasanan tulee olla vähintään 8 merkkiä"),
-										  validators.Regexp(".*[0-9].*[a-z].*[A-Z].*|.*[0-9].*[A-Z].*[a-z].*|.*[a-z].*[0-9].*[A-Z].*|.*[a-z].*[A-Z].*[0-9].*|.*[A-Z].*[0-9].*[a-z].*|.*[A-Z].*[0-9].*[a-z].*", message="Salasanassa tulee olla pieniä ja isoja kirjaimia sekä numeroita")])
+										  validators.Regexp(".*[0-9].*[a-z].*[A-Z].*|.*[0-9].*[A-Z].*[a-z].*|.*[a-z].*[0-9].*[A-Z].*|.*[a-z].*[A-Z].*[0-9].*|.*[A-Z].*[0-9].*[a-z].*|.*[A-Z].*[a-z].*[0-9].*", message="Salasanassa tulee olla pieniä ja isoja kirjaimia sekä numeroita")])
 	password_confirmation = PasswordField("Salasana uudelleen")
 
 	class Meta:
